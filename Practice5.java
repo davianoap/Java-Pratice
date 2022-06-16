32e0b82c7739ad7c8596bd1ea5199ca22eef169d
Daviano Anugrah .P <daviano123@gmail.com>
2022-06-16 19:18:42 +0700

    add practice4

---
+++ Practice5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Practice5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Masukkan nilai A : ");
+        int a = sc.nextInt();
+        System.out.println("Masukkan nilai B : ");
+        int b = sc.nextInt();
+
+        int c = a*b;
+
+        System.out.println(c);
+    }
+}
